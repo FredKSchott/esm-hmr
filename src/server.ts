93bddbf1b0ba377c27341bbd1e53af4b01f587e5
Fred K. Schott <fkschott@gmail.com>
2020-05-20 18:14:58 -0700

    update: snowpack 2.0.0-rc.3 hmr logic

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,101 @@
-// Currently, the server implementation lives in Snowpack:
-// https://github.com/pikapkg/snowpack/blob/master/src/hmr-server-engine.ts
-// We will eventually move it into here to be shared more generally.
+import http from 'http';
+
+interface Dependency {
+  dependents: Set<string>;
+  dependencies: Set<string>;
+  isHmrEnabled: boolean;
+  needsReplacement: boolean;
+}
+
+function sendMessage(res: http.ServerResponse, channel: string, data: string) {
+  res.write(`event: ${channel}\nid: 0\ndata: ${data}\n`);
+  res.write('\n\n');
+}
+
+export class EsmHmrEngine {
+  clients: http.ServerResponse[] = [];
+  dependencyTree = new Map<string, Dependency>();
+
+  createEntry(sourceUrl: string) {
+    const newEntry: Dependency = {
+      dependencies: new Set(),
+      dependents: new Set(),
+      needsReplacement: false,
+      isHmrEnabled: false,
+    };
+    this.dependencyTree.set(sourceUrl, newEntry);
+    return newEntry;
+  }
+
+  getEntry(sourceUrl: string, createIfNotFound = false) {
+    const result = this.dependencyTree.get(sourceUrl);
+    if (result) {
+      return result;
+    }
+    if (createIfNotFound) {
+      return this.createEntry(sourceUrl);
+    }
+    return null;
+  }
+
+  setEntry(sourceUrl: string, imports: string[], isHmrEnabled: boolean) {
+    const result = this.getEntry(sourceUrl, true)!;
+    const outdatedDependencies = new Set(result.dependencies);
+    result.isHmrEnabled = isHmrEnabled;
+    for (const importUrl of imports) {
+      this.addRelationship(sourceUrl, importUrl);
+      outdatedDependencies.delete(importUrl);
+    }
+    for (const importUrl of outdatedDependencies) {
+      this.removeRelationship(sourceUrl, importUrl);
+    }
+  }
+
+  removeRelationship(sourceUrl: string, importUrl: string) {
+    let importResult = this.getEntry(importUrl);
+    importResult && importResult.dependents.delete(sourceUrl);
+    const sourceResult = this.getEntry(sourceUrl);
+    sourceResult && sourceResult.dependencies.delete(importUrl);
+  }
+
+  addRelationship(sourceUrl: string, importUrl: string) {
+    if (importUrl !== sourceUrl) {
+      let importResult = this.getEntry(importUrl, true)!;
+      importResult.dependents.add(sourceUrl);
+      const sourceResult = this.getEntry(sourceUrl, true)!;
+      sourceResult.dependencies.add(importUrl);
+    }
+  }
+
+  markEntryForReplacement(entry: Dependency, state: boolean) {
+    entry.needsReplacement = state;
+  }
+
+  broadcastMessage(channel: string, data: object) {
+    for (const client of this.clients) {
+      sendMessage(client, channel, JSON.stringify(data));
+    }
+  }
+
+  connectClient(res: http.ServerResponse) {
+    res.writeHead(200, {
+      Connection: 'keep-alive',
+      'Content-Type': 'text/event-stream',
+      'Cache-Control': 'no-cache',
+      'Access-Control-Allow-Origin': '*',
+    });
+    sendMessage(res, 'connected', 'ready');
+    setInterval(sendMessage, 60000, res, 'ping', 'waiting');
+    this.clients.push(res);
+  }
+
+  disconnectClient(client: http.ServerResponse) {
+    this.clients.splice(this.clients.indexOf(client), 1);
+  }
+
+  disconnectAllClients() {
+    for (const client of this.clients) {
+      client.end();
+    }
+  }
+}
